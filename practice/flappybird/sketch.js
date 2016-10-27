cf122b3d582259032b28db7380c2e20b048b111c
Collin Vossman <cvossman@ksu.edu>
2016-10-27 18:14:16 -0500

    test github desktop

---
+++ practice/flappybird/sketch.js
@@ -0,0 +1,66 @@
+var bird;
+var pipes = [];
+var state = 'start';
+var score = 0;
+function setup() {
+  createCanvas(400,600);
+  bird = new Bird();
+  pipes.push(new Pipe());
+}
+
+function draw() {
+  background(0);
+  
+  if (state == 'start') {
+    textSize(32);
+    fill(0, 102, 153);
+    text("Flappy Bird", 100, 50);
+    text("Press Enter to start!", 50, 500);
+  }
+  
+  if (state == 'playing') {
+    for (var i = pipes.length - 1; i >= 0; i--){
+      pipes[i].show();
+      pipes[i].update();
+      
+      if (pipes[i].hits(bird)) {
+        console.log("HIT");
+        state = 'gameover';
+      }
+      
+      // Continue fixing score
+      if (frameCount % 100 == 0 && !pipes[i].hits(bird)) {
+        console.log("SCORE");
+        score++;
+      }
+      
+      if (pipes[i].offscreen()) {
+        pipes.splice(i, 1);
+      }
+    }
+    
+    bird.update();
+    bird.show();
+    
+    if (frameCount % 100 == 0) {
+      pipes.push(new Pipe());
+    }
+  }
+  
+  if (state == 'gameover') {
+    textSize(32);
+    fill(0, 102, 153);
+    text("Game Over!", 100, 300);
+    text("Score: " + (score - 1)/2, 120, 400); // fix score
+    text("Refresh to restart!", 40, 500);
+  }
+}
+
+function keyPressed() {
+  if (state == 'start' && keyCode == "13") {
+    state = 'playing';
+  }
+  if (state == 'playing' && key == ' ') {
+    bird.up();
+  }
+}
